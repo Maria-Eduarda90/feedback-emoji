3e0422b99d13e7b3daa51d350dfafceb6f2c9a88
Mary Skywalker <snowdennaruto90@gmail.com>
2023-01-29 11:06:27 -0400

    cors added and routes import

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,12 @@
 import express from "express";
+import cors from 'cors';
+
+import { router } from "./routes/routes";
 
 const app = express();
+app.use(cors());
+
+app.use(express.json());
+app.use(router);
 
 app.listen(4000, () => console.log('Server is running on PORT 4000'));
