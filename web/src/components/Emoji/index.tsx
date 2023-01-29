2d37d204e1371c277e7b3d7d5146250de65fb4bd
Mary Skywalker <snowdennaruto90@gmail.com>
2023-01-28 22:13:44 -0400

    passing the value to the component

--- web/src/components/Emoji/index.tsx
+++ web/src/components/Emoji/index.tsx
@@ -1,25 +1,28 @@
 import styles from './styles.module.scss';
+import { EmojiTypes } from '../../@types/EmojiTypes';
+import { EnumEmoji } from '../../enum/enum-emoji';
 
-export function Emojis(){
+export function Emojis({ value, setValue }: EmojiTypes){
+    
     return(
         <main className={styles.emojis}>
             <div>
-                <button>👍🏻</button>
+                <button value={value} onClick={() => setValue(EnumEmoji.COOL)}>👍🏻</button>
             </div>
             <div>
-                <button>😝</button>
+                <button value={value} onClick={() => setValue(EnumEmoji.NOTBAD)}>😝</button>
             </div>
             <div>
-                <button>😍</button>
+                <button value={value} onClick={() => setValue(EnumEmoji.LOVEDIT)}>😍</button>
             </div>
             <div>
-                <button>😮</button>
+                <button value={value} onClick={() => setValue(EnumEmoji.WOW)}>😮</button>
             </div>
             <div>
-                <button>😤</button>
+                <button value={value} onClick={() => setValue(EnumEmoji.BAD)}>😤</button>
             </div>
             <div>
-                <button>😥</button>
+                <button value={value} onClick={() => setValue(EnumEmoji.TERRIBLE)}>😥</button>
             </div>
         </main>
     );
