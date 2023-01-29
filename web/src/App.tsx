3b6139deacbf2dcd6eff8149bb3a4b315089bb85
Mary Skywalker <snowdennaruto90@gmail.com>
2023-01-28 22:14:38 -0400

    wip

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import { Emojis } from "./components/Emoji";
+import { EnumEmoji } from "./enum/enum-emoji";
 
 export function App() {
+  const [emoji, setEmoji] = useState("");
+  console.log(emoji)
+
   return (
     <section className="app">
       <h1>FEEDBACK - EMOJI</h1>
-      <Emojis />
+      <Emojis value={emoji} setValue={setEmoji}/>
     </section>
   )
 }
