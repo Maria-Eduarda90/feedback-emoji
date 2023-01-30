780cf00172aa1c3584fdb6dd42efb151f2d974c6
Mary Skywalker <snowdennaruto90@gmail.com>
2023-01-29 22:08:41 -0400

    feat: function creation for when the user clicks on any emoji save in the backend

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,26 @@
-import { useState } from "react";
+import { useState, FormEvent } from "react";
+import { api } from "./api/api";
 import { Emojis } from "./components/Emoji";
 
 export function App() {
   const [emoji, setEmoji] = useState("");
-  console.log(emoji)
+  
+  const handleSendReactionEmoji = async(e: FormEvent) => {
+    e.preventDefault();
+
+    const data = {
+      reaction: emoji,
+    }
+
+    await api.post('/reaction', data);
+  }
 
   return (
     <section className="app">
       <h1>FEEDBACK - EMOJI</h1>
-      <Emojis value={emoji} setValue={setEmoji}/>
+      <form onSubmit={handleSendReactionEmoji}>
+        <Emojis value={emoji} setValue={setEmoji} />
+      </form>
     </section>
   )
 }
